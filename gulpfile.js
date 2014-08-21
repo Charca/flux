4d6b1f68694b272d97e2abcc433d5317183a4bef
Maxi Ferreira <charca@gmail.com>
2014-08-21 01:44:14 -0300

    Added dispatcher and app-dispatcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,21 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
-var concat = require('concat');
+var browserSync = require('browser-sync');
+var concat = require('gulp-concat');
 
 gulp.task('browserify', function() {
   gulp.src('src/js/main.js')
     .pipe(browserify({transform: 'reactify'}))
     .pipe(concat('main.js'))
     .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('browser-sync', function() {
+  browserSync.init(['dist/**'], {
+    server: {
+      baseDir: ['dist', 'src']
+    }
+  });
 });
 
 gulp.task('copy', function() {
@@ -16,6 +25,6 @@ gulp.task('copy', function() {
 
 gulp.task('default', ['browserify', 'copy']);
 
-gulp.task('watch', function() {
+gulp.task('watch', ['browser-sync'], function() {
   gulp.watch('src/**/*.*', ['default']);
 });
